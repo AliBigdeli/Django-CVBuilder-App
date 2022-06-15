72fedf8106bd5403bf6cca0c8bec732382f08b18
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-06-15 09:38:17 +0400

    moving project

---
+++ core/dashboard/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+# class SkillAdmin(admin.ModelAdmin):
+#     list_display = ['id','name','get_titles']
+    
+#     def get_titles(self,obj):
+#         return ",".join([p.name for p in obj.job_title.all()])
+        
+
+
+
+# admin.site.register(Skill,SkillAdmin)
---
+++ core/dashboard/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+app_name = "dashboard"
+
+urlpatterns = [
+    # path("profile/",views.DashboardIndex.as_view(),name="index"),
+    # path("resume/",views.DescriptionView.as_view(),name="description-view"),
+    # path("resume/create/",views.DescriptionView.as_view(),name="description-view"),
+    # path("resume/profile/",views.SkillView.as_view(),name="skill-view"),
+
+    
+]
+
